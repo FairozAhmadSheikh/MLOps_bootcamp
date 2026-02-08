fe38047f2678a52ba276192237791e773407b535
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2026-02-08 16:54:44 +0530

    using html tags

--- Flask/main.py
+++ Flask/main.py
@@ -6,6 +6,12 @@ app=Flask(__name__)
 def home():
     return "Welcome to home page"
 
+# using html tags 
+
+@app.route("/homepage")
+def homepage():
+    return "<html><h1>This is a web page </h1></html>"
+
 if __name__=="__main__":
     app.run(debug=True)
 

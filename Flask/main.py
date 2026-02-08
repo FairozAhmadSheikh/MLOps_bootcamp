096bc2fa911543e4afb32ee2e20bdc29fddc27e6
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2026-02-08 16:58:07 +0530

    loaded page using render template

--- Flask/main.py
+++ Flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
@@ -11,6 +11,12 @@ def home():
 @app.route("/homepage")
 def homepage():
     return "<html><h1>This is a web page </h1></html>"
+
+
+# Using Templates
+@app.route("/load_from_template")
+def load_temp():
+    return render_template("loaded.html")
 
 if __name__=="__main__":
     app.run(debug=True)

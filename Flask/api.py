ffca8fed9f06b0ec121b777465b5ed5ee9240766
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2026-02-12 14:16:52 +0530

    starting mlflow

--- Flask/api.py
+++ Flask/api.py
@@ -54,7 +54,7 @@ def update_item(item_id):
         return jsonify({"result":"Item Updated"})
     
 # Delete 
-@app.route('items/<int:item_id>',methods=['DELETE'])
+@app.route('/items/<int:item_id>',methods=['DELETE'])
 def delete_item(item_id):
     global items
     items=[item for item in items if item['id'] != item_id]
